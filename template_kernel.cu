6625fc861ef1d2281663ed656300ee3a82a28b85
Rok Roskar <roskar@physik.uzh.ch>
2013-10-28 18:13:58 +0100

    update some stuff

--- template_kernel.cu
+++ template_kernel.cu
@@ -8,6 +8,13 @@
 #define PI 3.141592654f
 #define PI_I 1.0/PI
 
+struct Coords {
+  float xmin;
+  float xmax;
+  float ymin;
+  float ymax;
+};
+
 __device__ void kernel_func(float *kernel, float h, float max_d)
 {
   /* 
@@ -103,7 +110,7 @@ __device__ void update_image(float *global, float *local, int x_offset, int y_of
   my_start = idx*pix_per_thread;
   my_end = my_start + pix_per_thread;
   // if this is the last thread, make it take the rest of the pixels
-  if (idx == blockDim.x*blockIdx.x-1) my_end = nx_loc*ny_loc;
+  if (idx == blockDim.x-1) my_end = nx_loc*ny_loc;
   
   for(int p = my_start; p < my_end; p++) 
     {
@@ -113,36 +120,67 @@ __device__ void update_image(float *global, float *local, int x_offset, int y_of
     }
 }
 
-__global__ void tile_render_kernel(float *xs, float *ys, float *qts, float *hs, int *inds, int Npart,
-                                   int kmin, int kmax, 
-                                   float xmin_p, float xmax_p, float ymin_p, float ymax_p,
-                                   int xmin, int xmax, int ymin, int ymax, 
+
+__global__ void tile_render_kernel(float *xs, float *ys, float *qts, float *hs, int *inds, int *p_per_tile,
+                                   float xmin, float xmax, float ymin, float ymax, 
                                    float *global_image, int nx_glob, int ny_glob)
 {    
 
   int  Nthreads = blockDim.x;
   int idx = threadIdx.x;
-
+  
   
   // declare shared arrays -- image and base kernel
   __shared__ float local_image[IMAGE_XDIM*IMAGE_YDIM];
   __shared__ float kernel[KSIZE*KSIZE];
 
-  int nx = xmax-xmin+1;
-  int ny = ymax-ymin+1;
+  int nx;
+  int ny;
   
-  float dx = (xmax_p-xmin_p)/float(nx);
-  float dy = (ymax_p-ymin_p)/float(ny);
+  float dx,dy; 
+
+  Coords tile_pix, tile_phy; // pixel and physical limits of the tile processed by the block
+  Coords global_coords;
 
   float i_max_d;
   
   float max_d_curr = 0.0, i_h_cb;
   int start_ind = 0, end_ind = 0;
-  
+  int kmin=1,kmax=31;
   int i,j,pind,Nper_kernel,Nper_thread,my_start = 0,my_end=0;
   int left,upper,xpos,ypos;
   float x,y,qt,loc_val,ker_val;
+  int ind_offset = 0;
 
+  global_coords.xmin = xmin;
+  global_coords.xmax = xmax;
+  global_coords.ymin = ymin;
+  global_coords.ymax = ymax;
+  
+  dx = (global_coords.xmax-global_coords.xmin)/float(nx_glob);
+  dy = (global_coords.ymax-global_coords.ymin)/float(ny_glob);
+  /* 
+    figure out which part of the image this block is responsible for
+  */
+
+  tile_pix.xmin = blockIdx.x*IMAGE_XDIM;
+  tile_pix.ymin = blockIdx.x*IMAGE_YDIM;
+  tile_pix.xmax = (blockIdx.x+1)*IMAGE_XDIM - 1 ? blockIdx.x < gridDim.x -1 : nx_glob-1;
+  tile_pix.ymax = (blockIdx.x+1)*IMAGE_YDIM - 1 ? blockIdx.x < gridDim.x -1 : ny_glob-1;
+  
+  nx = tile_pix.xmax-tile_pix.xmin;
+  ny = tile_pix.ymax-tile_pix.ymin;
+
+  tile_phy.xmin = global_coords.xmin + dx*tile_pix.xmin;
+  tile_phy.ymin = global_coords.ymin + dy*tile_pix.ymin;
+  tile_phy.xmax = global_coords.xmin + dx*(tile_pix.xmax+1);
+  tile_phy.ymin = global_coords.ymin + dy*(tile_pix.ymax+1);
+  
+  
+
+  // set up the index array offset
+  for(i=0;i<blockIdx.x;i++) ind_offset += p_per_tile[i];
+  start_ind = ind_offset;
 
   /*
     ------------------------------
@@ -151,15 +189,18 @@ __global__ void tile_render_kernel(float *xs, float *ys, float *qts, float *hs, 
   */
   
   // make sure kmin and kmax are odd
-  if (!(kmax % 2)) kmax += 1;
-  if (!(kmin % 2)) kmin += 1;
-  kmin = (kmin>1) ? kmin : 1;
+  //if (!(kmax % 2)) kmax += 1;
+  //if (!(kmin % 2)) kmin += 1;
+  //kmin = (kmin>1) ? kmin : 1;
 
-  for(i=0;i<IMAGE_SIZE;i++) local_image[i]=0.0;
-  
-  //  if (idx==0) printf("max/min = %d %d\n", xmax, xmin);
+  if(idx < IMAGE_SIZE) for(i=idx;i<IMAGE_SIZE;i+=Nthreads) local_image[i]=0.0;
+      
+  if (idx==0) {
+    printf("block = %d min/max = %d %d\n", blockIdx.x, tile_phy.xmin, tile_phy.xmax);
+    printf("global min/max = %d %d\n", xmin,xmax);
+  }
   //for(int m=0;m<5000;m++){
-  for(int k=kmin; k < kmax+2; k+=2) 
+  for(int k=1; k <= 31; k+=2) 
     {
       __syncthreads();
       // set up the base kernel
@@ -178,7 +219,7 @@ __global__ void tile_render_kernel(float *xs, float *ys, float *qts, float *hs, 
          ------------------------------------------------- */
       
       /* DO THIS SEARCH IN PARALLEL */
-      for(end_ind=start_ind;end_ind<Npart;) { 
+      for(end_ind = start_ind; end_ind < p_per_tile[blockIdx.x]+ind_offset; ) { 
         if (2*hs[inds[end_ind]] < max_d_curr) end_ind++;
         else break;
       }
@@ -200,12 +241,14 @@ __global__ void tile_render_kernel(float *xs, float *ys, float *qts, float *hs, 
           my_start = Nper_thread*idx+start_ind;
           
           // if this is the last thread, make it pick up the slack
-          my_end = end_ind;
           if (idx == Nthreads-1) 
             my_end = end_ind;
           else 
             my_end = Nper_thread+my_start;
           
+          if (idx == 0) printf("k = %d Nperkernel = %d Nper_thread = %d\n", k, Nper_kernel, Nper_thread);
+          printf("idx = %d my start = %d my end = %d\n",idx, my_start,my_end);
+
           //all threads have their particle indices figured out, increment for next iteration
          
           start_ind = end_ind;
@@ -221,8 +264,8 @@ __global__ void tile_render_kernel(float *xs, float *ys, float *qts, float *hs, 
               //h = hs[inds[pind]];
               qt = qts[inds[pind]];
               
-              xpos = __float2int_rd((x-xmin_p)/dx);
-              ypos = __float2int_rd((y-ymin_p)/dy);
+              xpos = __float2int_rd((x-tile_phy.xmin)/dx);
+              ypos = __float2int_rd((y-tile_phy.ymin)/dy);
               
               left = xpos-k/2;
               upper = ypos-k/2;
@@ -245,7 +288,7 @@ __global__ void tile_render_kernel(float *xs, float *ys, float *qts, float *hs, 
     }
   __syncthreads();
   /* update global image */
-  update_image(global_image,local_image,xmin,ymin,nx_glob,nx,ny);
+  update_image(global_image,local_image,tile_pix.xmin,tile_pix.ymin,nx_glob,nx,ny);
   //  }
 }
 
